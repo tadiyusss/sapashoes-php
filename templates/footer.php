d32e7a58f609cbbdbf2bb68df9dcb340aed78643
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-06-27 14:10:37 +0800

    reusable php template

---
+++ templates/footer.php
@@ -0,0 +1,28 @@
+<footer class="bg-zinc-950 p-4 roboto">
+    <div class="max-w-7xl mx-auto p-4">
+        <div class="flex flex-col md:flex-row md:space-y-0 space-y-8 text-white justify-between">
+            <div>
+                <h2 class="bebas-neue text-4xl">SapaShoes</h2>
+                <p class="text-sm text-zinc-400 font-medium">All rights reserved 2025</p>
+            </div>
+            <div>
+                <h2 class="text-xl font-medium">Quick Links</h2>
+                <ul class="space-y-1 mt-2 text-sm text-gray-200">
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Careers</li>
+                    <li>Store Locator</li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-xl font-medium">Store Policy</h2>
+                <ul class="space-y-1 mt-2 text-sm text-gray-200">
+                    <li>Shipping Policy</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms and Services</li>
+                    <li>Returns and Exchanges</li>
+                </ul>
+            </div>
+        </div>    
+    </div>
+</footer>
---
+++ templates/navigation.php
@@ -0,0 +1,24 @@
+<nav class="border-b bg-white">
+    <div class="bg-zinc-950 text-white p-2">
+        <div class="max-w-7xl mx-auto">
+            <ul class="flex items-center space-x-8 roboto">
+                <li>
+                    <a href="login.php">My Account</a>
+                </li>
+                <li>
+                    <a href="cart.php">Cart</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="max-w-7xl p-4 flex items-center justify-between mx-auto">
+        <h2 class="bebas-neue text-3xl">SapaShoes</h2>
+        <ul class="flex items-center space-x-4 roboto">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+</nav>
