be4f39244016bf5c4f5f59447aa0b1708bf0715d
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-06-27 14:09:56 +0800

    moved nav and footer to reusable php files

--- index.php
+++ index.php
@@ -8,27 +8,7 @@
         <link rel="stylesheet" href="assets/css/main.css">
     </head>
     <body>
-        <nav>
-            <div class="bg-zinc-950 text-white p-2">
-                <div class="max-w-7xl mx-auto">
-                    <ul class="flex items-center space-x-8 roboto">
-                        <li>
-                            <a href="login.php">My Account</a>
-                        </li>
-                        <li>Cart</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="max-w-7xl p-4 flex items-center justify-between mx-auto">
-                <h2 class="bebas-neue text-3xl">SapaShoes</h2>
-                <ul class="flex items-center space-x-4 roboto">
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-            
-        </nav>
+        <?php include 'templates/navigation.php'; ?>
         <img src="assets/images/carousel1.webp" class="w-full h-[400px] object-cover">
         <div class="space-y-8 my-12">
             <div class="max-w-7xl mx-auto px-6">
@@ -47,33 +27,6 @@
                 </div>
             </div>
         </div>
-        <footer class="bg-zinc-950 p-4 roboto">
-            <div class="max-w-7xl mx-auto p-4">
-                <div class="flex flex-col md:flex-row text-white justify-between">
-                    <div>
-                        <h2 class="bebas-neue text-4xl">SapaShoes</h2>
-                        <p class="text-sm text-zinc-400 font-medium">All rights reserved 2025</p>
-                    </div>
-                    <div>
-                        <h2 class="text-xl font-medium">Quick Links</h2>
-                        <ul class="space-y-1 mt-2 text-sm text-gray-200">
-                            <li>About Us</li>
-                            <li>Contact Us</li>
-                            <li>Careers</li>
-                            <li>Store Locator</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 class="text-xl font-medium">Store Policy</h2>
-                        <ul class="space-y-1 mt-2 text-sm text-gray-200">
-                            <li>Shipping Policy</li>
-                            <li>Privacy Policy</li>
-                            <li>Terms and Services</li>
-                            <li>Returns and Exchanges</li>
-                        </ul>
-                    </div>
-                </div>    
-            </div>
-        </footer>
+        <?php include 'templates/footer.php'; ?>
     </body>
 </html>
