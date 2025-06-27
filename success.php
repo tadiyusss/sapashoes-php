5d2ba678d34a1d033c372cc987b42fc92e072aea
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-06-27 15:24:49 +0800

    fixed responsiveness in success.php

--- success.php
+++ success.php
@@ -8,14 +8,14 @@
         <link rel="stylesheet" href="assets/css/main.css">
     </head>
     <body class="roboto">
-        <div class="max-w-4xl mx-auto space-y-2 mt-24 text-center">
+        <div class="max-w-4xl mx-auto space-y-2 mt-24 text-center px-4">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-20 text-green-700 mx-auto">
                 <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
             </svg>
-            <h1 class="text-4xl font-medium">Thank You for Your Purchase!</h1>
-            <p class="text-2xl text-gray-600">We've received your order and will begin processing it shortly. Stay tuned for updates on your delivery.</p>
+            <h1 class="md:text-4xl text-2xl font-medium">Thank You for Your Purchase!</h1>
+            <p class="md:text-2xl text-lg text-gray-600">We've received your order and will begin processing it shortly. Stay tuned for updates on your delivery.</p>
             <div class="pt-8">
-                <a href="index.php" class="mx-auto px-8 py-2 rounded bg-zinc-800 hover:bg-zinc-900 ease duration-200 text-white">Continue Shopping</a>
+                <a href="index.php" class="mx-auto md:px-8 px-4 py-2 md:text-base text-sm rounded bg-zinc-800 hover:bg-zinc-900 ease duration-200 text-white">Continue Shopping</a>
             </div>
         </div>
     </body>
