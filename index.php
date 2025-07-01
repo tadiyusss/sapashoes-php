b1da8fe4a813ef35976aab2b428ee01e603cb4a2
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-01 10:16:45 +0800

    show only products that have stocks > 0

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
     require_once 'config.php';
-    $result = mysqli_query($conn, "SELECT * FROM products ORDER BY id DESC");
+    $result = mysqli_query($conn, "SELECT * FROM products WHERE stocks > 0 ORDER BY id DESC");
     $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
