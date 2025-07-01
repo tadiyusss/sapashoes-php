ffdb24c474a8d08d059a38c2ed0fb38162cf06f5
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-01 10:31:18 +0800

    checkout funciton

--- cart.php
+++ cart.php
@@ -1,6 +1,9 @@
 <?php 
     require 'config.php';
+    require_once 'utils/flash.php';
+
     $total_price = 0;
+    $error_message = get_flash('error');
     function get_product_by_id($product_id, $conn) {
         $stmt = $conn->prepare("SELECT * FROM products WHERE id = ?");
         $stmt->bind_param("i", $product_id);
@@ -15,12 +18,84 @@
         header('Location: login.php');
     }
 
-    $stmt = $conn->prepare("SELECT * FROM cart WHERE owner_id = ? ORDER BY id DESC");
+    $stmt = $conn->prepare("SELECT * FROM products JOIN cart ON products.id = cart.product_id WHERE cart.owner_id = ? AND products.stocks > 0 ORDER BY cart.id DESC;"); 
     $stmt->bind_param("i", $_SESSION["user_id"]);
     $stmt->execute();
     $result = $stmt->get_result();
     $cart_items = $result->fetch_all(MYSQLI_ASSOC);
-    $stmt->close();
+
+    if($_SERVER['REQUEST_METHOD'] === "POST"){
+        $card_number = $_POST['card_number'];
+        $expiry_month = $_POST['expiry_month'];
+        $expiry_year = $_POST['expiry_year'];
+        $cvv = $_POST['cvv'];
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+        $city = $_POST['city'];
+
+        if (empty($card_number) || empty($expiry_month) || empty($expiry_year) || empty($cvv) || empty($name) || empty($address) || empty($city)) {
+            set_flash('error', 'All fields are required.');
+            header('Location: cart.php');
+            exit;
+        }
+
+        # check if expiry_date is valid
+        $expiry_date = DateTime::createFromFormat('Y-m', "$expiry_year-$expiry_month");
+        if (!$expiry_date || $expiry_date < new DateTime()) {
+            set_flash('error', 'Invalid expiry date.');
+            header('Location: cart.php');
+            exit;
+        }
+
+        # check if cvv is valid
+        if (!preg_match('/^\d{3,4}$/', $cvv)) {
+            set_flash('error', 'Invalid CVV.');
+            header('Location: cart.php');
+            exit;
+        }
+
+        # check if card_number is valid
+        if (!preg_match('/^\d{16}$/', $card_number)) {
+            set_flash('error', 'Invalid card number.');
+            header('Location: cart.php');
+            exit;
+        }
+
+        # get all cart items that are not out of stock
+        $total = 0;
+        $stmt = $conn->prepare("SELECT * FROM products JOIN cart ON products.id = cart.product_id WHERE cart.owner_id = ? AND products.stocks > 0 ORDER BY cart.id DESC;"); 
+        $stmt->bind_param("i", $_SESSION["user_id"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $cart_items = $result->fetch_all(MYSQLI_ASSOC);
+        foreach ($cart_items as $cart_item){
+            # -1 stock for each product in the cart
+            $total += $cart_item['price'];
+            $product_id = $cart_item['product_id'];
+            $stmt = $conn->prepare("UPDATE products SET stocks = stocks - 1 WHERE id = ?");
+            $stmt->bind_param("i", $product_id);
+            $stmt->execute();
+            $stmt->close();
+
+            # delete the item from the cart
+            $stmt = $conn->prepare("DELETE FROM cart WHERE id = ?");
+            $stmt->bind_param("i", $cart_item['id']);
+            $stmt->execute();
+
+        }
+
+        # insert into sales table
+        $stmt = $conn->prepare("INSERT INTO sales (total, customer_name, address, city) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("dsss", $total, $name, $address, $city);
+        $stmt->execute();
+        $stmt->close();
+        # delete all items in the cart
+
+
+
+        
+        header('Location: success.php');
+    }
 
 ?>
 
@@ -43,7 +118,6 @@
                     </div>
                     <?php if (count($cart_items) > 0): ?>
                         <?php foreach ($cart_items as $cart_item): ?>
-                            <?php $product = get_product_by_id($cart_item['product_id'], $conn); ?>
                             <div class="border-b p-2 pb-4 flex items-center justify-between">
                                 <div class="flex items-center space-x-12">
                                     <a href="delete_cart.php?id=<?= $cart_item['id'] ?>">
@@ -51,17 +125,16 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                                         </svg>
                                     </a>
-                                    <img src="assets/images/shoes/<?= $product['image'] ?>" class="size-24 border rounded bg-white">
+                                    <img src="assets/images/shoes/<?= $cart_item['image'] ?>" class="size-24 border rounded bg-white">
                                     <div class="block space-y-2">
-                                        <h4 class="bebas-neue"><?= $product['name'] ?></h4>
+                                        <h4 class="bebas-neue"><?= $cart_item['name'] ?></h4>
                                     </div>
-                                    
                                 </div>
                                 <div class="block">
-                                    <p class="bebas-neue">₱ <?= $product['price'] ?></p>
+                                    <p class="bebas-neue">₱ <?= $cart_item['price'] ?></p>
                                 </div>
                             </div>
-                            <?php $total_price += $product['price']; ?>
+                            <?php $total_price += $cart_item['price']; ?>
                         <?php endforeach; ?>
                     <?php else: ?>
                         <div class="text-center p-4">
@@ -88,8 +161,8 @@
                             <div class="md:col-span-1 col-span-2">
                                 <label for="expiry_date" class="text-sm font-medium text-gray-600">Expiry Date</label>
                                 <div class="md:flex block items-center md:space-x-2 md:space-y-0 space-y-2">
-                                    <input type="number" name="expiry_date" id="expiry_date" class="w-full border ease duration-200 focus:outline-zinc-200 focus:ring-zinc-200 hover:outline-zinc-200 px-2 py-1 rounded appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="MM" min="1" max="12">
-                                    <input type="number" name="expiry_date" id="expiry_date" class="w-full border ease duration-200 focus:outline-zinc-200 focus:ring-zinc-200 hover:outline-zinc-200 px-2 py-1 rounded appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="YYYY" min="2023" max="2030">
+                                    <input type="number" name="expiry_month" id="expiry_date" class="w-full border ease duration-200 focus:outline-zinc-200 focus:ring-zinc-200 hover:outline-zinc-200 px-2 py-1 rounded appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="MM" min="1" max="12">
+                                    <input type="number" name="expiry_year" id="expiry_date" class="w-full border ease duration-200 focus:outline-zinc-200 focus:ring-zinc-200 hover:outline-zinc-200 px-2 py-1 rounded appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="YYYY" min="2023" max="2030">
                                 </div>
                             </div>
                             <div class="md:col-span-1 col-span-2">
@@ -108,6 +181,11 @@
                                 <label for="city" class="text-sm font-medium text-gray-600">City</label>
                                 <input type="text" name="city" id="city" class="w-full border ease duration-200 focus:outline-zinc-200 focus:ring-zinc-200 hover:outline-zinc-200 px-2 py-1 rounded appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none">
                             </div>
+                            <?php if ($error_message): ?>
+                                <div class="col-span-2 bg-red-600 p-2 text-white rounded">
+                                    <p><?= $error_message; ?></p>
+                                </div>
+                            <?php endif; ?>
                             <button type="submit" class="col-span-2 bg-zinc-800 hover:bg-zinc-900 ease duration-200 text-white w-full py-2 mt-4 rounded">Checkout</button>
                         </form>
                     </div>
