e2f8f40c7ee95a9df5ca8488e7c9845938b75a4d
tadiyusss <toledothaddeusf@gmail.com>
2025-07-03 06:20:48 +0800

    db design

--- sapashoes_db.sql
+++ sapashoes_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 02, 2025 at 09:53 PM
+-- Generation Time: Jul 02, 2025 at 10:20 PM
 -- Server version: 11.8.1-MariaDB-4
 -- PHP Version: 8.2.27
 
@@ -41,6 +41,132 @@ INSERT INTO `brands` (`id`, `brand_name`) VALUES
 (4, 'Adidas'),
 (5, 'Puma');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discount_code`
+--
+
+CREATE TABLE `discount_code` (
+  `id` int(11) NOT NULL,
+  `discount_code` varchar(255) NOT NULL,
+  `percentage` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `discount_code`
+--
+
+INSERT INTO `discount_code` (`id`, `discount_code`, `percentage`) VALUES
+(3, '30PERC', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(15,2) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `stocks` int(11) NOT NULL DEFAULT 1,
+  `brand_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `stocks`, `brand_name`) VALUES
+(3, 'Air Jordan 1 Retro High OG \'Yellow Ochre\'', 2599.00, '6864edb235609.webp', 87, 'Nike'),
+(5, 'ABIBAS', 1400.00, '686509ce6f36f.webp', 97, 'Puma');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `status` enum('Pending','Processing','Confirmed','Out for Delivery','Shipped','Cancelled') NOT NULL,
+  `sale_id` varchar(16) NOT NULL,
+  `discount_code` varchar(255) DEFAULT NULL,
+  `owner_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `total`, `customer_name`, `address`, `city`, `status`, `sale_id`, `discount_code`, `owner_id`) VALUES
+(12, 1819.30, 'Mark Beats', 'Molino 4', 'Bacoor', 'Shipped', 'APZ7zXH0fhIw3bSh', '30PERC', 2),
+(13, 2799.30, 'JM Mark', 'Molino 4', 'Bacoor', 'Shipped', 'QE7CPm2qwv5IzXgn', '30PERC', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sold_products`
+--
+
+CREATE TABLE `sold_products` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `size` int(11) NOT NULL,
+  `sale_id` varchar(16) NOT NULL,
+  `owner_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `sold_products`
+--
+
+INSERT INTO `sold_products` (`id`, `product_id`, `size`, `sale_id`, `owner_id`) VALUES
+(7, 3, 5, 'APZ7zXH0fhIw3bSh', 2),
+(8, 5, 5, 'QE7CPm2qwv5IzXgn', 2),
+(9, 3, 6, 'QE7CPm2qwv5IzXgn', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `type` enum('user','admin') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `username`, `password_hash`, `type`) VALUES
+(2, 'thaddeustoledo@gmail.com', 'admin', '$2y$10$Fays4ruYw3AMelp2r47uMO.M3ilQOskHBwILm4UDXmSNny2JEd8L6', 'admin'),
+(5, 'thaddeus@example.com', 'thaddeus', '$2y$10$mRDMOy1Jigf2na3rQ5yoleLvIxCloVDoEA5JJO7j.3ff9fZMQ3MPS', 'user'),
+(6, 'doc@example.com', 'doc', '$2y$10$Li36yQRRvu2B32GNVRpCQOtDFUOyiaKsW59msGVrzeOc4Po/V2dc6', 'admin');
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +178,42 @@ ALTER TABLE `brands`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `discount_code`
+--
+ALTER TABLE `discount_code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sold_products`
+--
+ALTER TABLE `sold_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +222,42 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `brands`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `discount_code`
+--
+ALTER TABLE `discount_code`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `sold_products`
+--
+ALTER TABLE `sold_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
