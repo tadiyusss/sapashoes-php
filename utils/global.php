2abe7603da8c2df79dfb2973ab664cb226ff0f19
tadiyusss <toledothaddeusf@gmail.com>
2025-07-03 05:54:52 +0800

    global user defined functions

---
+++ utils/global.php
@@ -0,0 +1,17 @@
+<?php 
+
+    function random_string($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+
+    function format_number($number) {
+        return number_format((float)$number, 2, '.', ',');
+    }
+?>
