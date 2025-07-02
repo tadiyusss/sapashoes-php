fcd4d8cb805bd709a82a6e2849050dbc57b36098
tadiyusss <toledothaddeusf@gmail.com>
2025-07-02 17:11:36 +0800

    added filtered by brand name

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
 
     require_once 'config.php';
-    $result = mysqli_query($conn, "SELECT * FROM products WHERE stocks > 0 ORDER BY id DESC");
-    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    if (isset($_GET['brand'])) {
+        $stmt = mysqli_prepare($conn, "SELECT * FROM products WHERE brand_name = ? ORDER BY id DESC");
+        mysqli_stmt_bind_param($stmt, 's', $_GET['brand']);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        $result = mysqli_query($conn, "SELECT * FROM products ORDER BY id DESC");
+        $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+
+    $result = mysqli_query($conn, "SELECT * FROM brands ORDER BY id DESC");
+    $brands = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
 <!doctype html>
@@ -18,6 +29,11 @@
         <?php include 'templates/navigation.php'; ?>
         <img src="assets/images/carousel1.webp" class="w-full h-[400px] object-cover">
         <div class="space-y-8 my-12">
+            <div class="grid md:grid-cols-4 grid-cols-2 gap-4 max-w-7xl mx-auto">
+                <?php foreach ($brands as $brand): ?>
+                    <a href="index.php?brand=<?= htmlspecialchars($brand['brand_name']); ?>" class="border px-2 text-center py-4 rounded bg-gray-50 shadow"><?= htmlspecialchars($brand['brand_name']); ?></a>
+                <?php endforeach; ?>
+            </div>
             <div class="max-w-7xl mx-auto px-6">
                 <h4 class="bebas-neue text-4xl">New Arrivals</h4>
                 <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
